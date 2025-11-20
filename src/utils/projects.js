13b12d61f4cbdb21484f9108ad324c6651496727
Vikas Koladiya <koladiyavikas2005@gmail.com>
2025-11-20 22:51:38 +0530

    Update Experience and add new projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,7 +2,20 @@ import ECommerceLogo from "../assets/images/ecommercelogo.webp";
 import ResultPortalLogo from "../assets/images/resultlogo.png";
 import JavaScriptProjectsLogo from "../assets/images/javascriptprojectlogo.jpg";
 import WebProjectLogo from "../assets/images/webprojectlogo.jpg";
+import AICareerGuruLogo from "../assets/images/AICareerGuruLogo.jpg";
+import BitLinkLogo from "../assets/images/BitLinkLogo.jpg";
 const data = [
+  {
+    name: "AI Career Guru",
+    type: "WebApp",
+    url: "https://guideaiguru.vercel.app/",
+    github: "https://github.com/VikasKoladiya/Ai-Career-Guru",
+    image: AICareerGuruLogo,
+    slug: "ai-career-guru",
+    description:
+      "An AI-Powered Guidance Platform that delivers interactive advice and intelligent support, facilitating seamless decision-making through conversational insights and data-driven recommendations.",
+    tech: ["Next Js", "Node Js", "Express Js", "PostgreSQL", "OpenAI API"],
+  },
   {
     name: "E Commerce App",
     type: "WebApp",
@@ -26,6 +39,17 @@ const data = [
     tech: ["HTML", "CSS", "PHP", "MySql"],
   },
   {
+    name: "Bit Link",
+    type: "WebApp",
+    url: "https://bitlink-smartlink.vercel.app/",
+    github: "https://github.com/VikasKoladiya/BitLink",
+    image: BitLinkLogo,
+    slug: "bit-link",
+    description:
+      "A smart link-management platform that lets users create, customize, and track short links, offering an easy dashboard with organized link control and quick performance analytics.",
+    tech: ["React Js", "Node Js", "Express Js", "MongoDB"],
+  },
+  {
     name: "JavaScript Projects",
     type: "WebApp",
     url: "https://github.com/VikasKoladiya/Javascript-Project",
@@ -34,11 +58,7 @@ const data = [
     slug: "javascript-projects",
     description:
       "A JavaScript Projects Repository showcasing various interactive and functional web applications built using JavaScript.",
-    tech: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-    ],
+    tech: ["HTML", "CSS", "JavaScript"],
   },
   {
     name: "Web Projects",
@@ -49,11 +69,7 @@ const data = [
     slug: "web-projects",
     description:
       "A Web Projects Repository featuring a collection of dynamic and responsive websites built using modern web technologies.",
-    tech: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-    ],
+    tech: ["HTML", "CSS", "JavaScript"],
   },
 ];
 
